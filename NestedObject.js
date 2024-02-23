c3fb87aecd8655099e6f6b00b21226dc55d08815
Siddharth Payghan <83947937+Sidpay@users.noreply.github.com>
2024-02-23 18:14:05 +0530

    Update NestedObject.js

--- NestedObject.js
+++ NestedObject.js
@@ -5,3 +5,4 @@ const employees = {
  "Ravi": {     "id": 2,     "Contact no": "8373963523",     "Location": "Banglore",    "salary": 90000,"address":{
    "City":"Banglore","State":"Karnataka"}
  } };
+ console.log(employees);
