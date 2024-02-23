8cce65dafa3c7317c67dfd11339c495368e3956e
Siddharth Payghan <83947937+Sidpay@users.noreply.github.com>
2024-02-23 17:54:35 +0530

    Update Boolean.js

--- Boolean.js
+++ Boolean.js
@@ -1,3 +1,5 @@
-function boolean(num){
-  return num % 10 === 0 ? true : false;
-}
+const isDivisibleBy10 = (number) => number % 10 === 0;
+const number1 = 2;
+const number2 = 30;
+console.log(${number1} is divisible by 10: ${isDivisibleBy10(number1)});
+console.log(${number2} is divisible by 10: ${isDivisibleBy10(number2)});
