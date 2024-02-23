a0e1948e51ff5a81f1c3652669e2eb2c47f5dd53
Siddharth Payghan <83947937+Sidpay@users.noreply.github.com>
2024-02-23 17:32:05 +0530

    Create NestedObject.js

---
+++ NestedObject.js
@@ -0,0 +1,7 @@
+const employees = {   
+  "Sid": {     "id": 1,     "Contact no": 7368278377,     "Location": "Pune",     "salary": 100000 ,"address":{
+    "City":"pune","State":"Maharashtra"}
+         },   
+ "Ravi": {     "id": 2,     "Contact no": "8373963523",     "Location": "Banglore",    "salary": 90000,"address":{
+   "City":"Banglore","State":"Karnataka"}
+ } };
